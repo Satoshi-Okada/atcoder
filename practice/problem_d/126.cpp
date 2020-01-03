85f4ef02515b0d296c34cdccb3059617f1fbd53b
Satoshi-Okada <satoshi2512eminem@gmail.com>
2020-01-03 23:44:57 +0900

    test

--- practice/problem_d/126.cpp
+++ practice/problem_d/126.cpp
@@ -6,3 +6,15 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define PI 3.14159265358979323846
 #define sz(x) ((int)(x).size())
+
+struct edge
+{
+    int to;
+    int length;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+}
