1fbf98d3c6bf5abf6d31eaf4fd9652d213de35dc
Satoshi-Okada <satoshi2512eminem@gmail.com>
2020-01-18 16:56:36 +0900

    20200118

--- practice/problem_c/119.cpp
+++ practice/problem_c/119.cpp
@@ -12,4 +12,64 @@ const int MOD = 1e9 + 7;
 const ll LINF = 1e18;
 int main()
 {
+    int n, a, b, c;
+    cin >> n >> a >> b >> c;
+    vector<int> l(n);
+
+    rep(i, n)
+    {
+        cin >> l[i];
+    }
+    //a1,b2,c3,null0
+
+    vector<int> flag(n);
+    int mn = INF;
+    rep(i, (int)pow(4, n))
+    {
+        int temp = i;
+        int temp_a = 0;
+        int temp_b = 0;
+        int temp_c = 0;
+        int counter = 0;
+        int flag;
+        for (int j = n - 1; j >= 0; j--)
+        {
+
+            flag = temp / (int)pow(4, j);
+            temp -= flag * (int)pow(4, j);
+            if (flag == 3)
+            {
+                if (temp_c != 0)
+                {
+                    counter++;
+                }
+                temp_c += l[j];
+            }
+            else if (flag == 2)
+            {
+                if (temp_b != 0)
+                {
+                    counter++;
+                }
+                temp_b += l[j];
+            }
+            else if (flag == 1)
+            {
+                if (temp_a != 0)
+                {
+                    counter++;
+                }
+                temp_a += l[j];
+            }
+        }
+        if (temp_a == 0 || temp_b == 0 || temp_c == 0)
+        {
+            continue;
+        }
+        int hypo = counter * 10 + abs(a - temp_a) + abs(b - temp_b) + abs(c - temp_c);
+        //cout << hypo << endl;
+        mn = min(hypo, mn);
+    }
+
+    cout << mn << endl;
 }
