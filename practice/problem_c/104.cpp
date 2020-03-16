9d1bd757228b6d77c4eaebb4bbbf699071616c92
Satoshi <satoshi@Satoshi-no-MacBook-Air.local>
2020-03-16 17:21:04 +0900

    change

---
+++ practice/problem_c/104.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define MOD 1000000007
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define PI 3.14159265358979323846
+#define sz(x) ((int)(x).size())
+
+int main()
+{
+    int d,g;
+    cin >> d >> g;
+    rep(i,n)
+    {
+        
+    }
+}
